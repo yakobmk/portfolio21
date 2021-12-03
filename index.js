366849e0b2329fc38c81c207316014e52f857640
yakobmk <yakobmk98@gmail.com>
2021-12-03 15:11:10 -0500

    initial completion

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const something = () => {
+    window.addEventListener('scroll', () => {
+        let content = document.querySelectorAll('.hidden');
+        let windowHeight = window.innerHeight;
+        console.log(content);
+
+        for (var i = 0; i<content.length; i++){
+            var element = content[i];
+            let contentPosition = element.getBoundingClientRect().top + 10;
+            let screenPosition = windowHeight;
+            console.log(screenPosition, "screen Position");
+            console.log(contentPosition, "content Position", element)
+            if (contentPosition <= screenPosition){
+                // console.log(element, "element in if");
+                element.classList.add('animate__animated', 'animated');
+                element.classList.remove('hidden');
+                // setTimeout(scrolling, 1000, element);
+            }
+        }
+    }) 
+    
+}
+
+// const scrolling = () => {
+//     console.log('in here');
+//     console.log(element, "element in scrolling");
+//     document.querySelector('.hidden').classList.add('animated')
+//     console.log(document.querySelector('.heading'))
+// }
+
+
+something()
+
