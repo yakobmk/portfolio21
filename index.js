244368d19968df1ee3ec67585099d208610eb5f0
yakobmk <yakobmk98@gmail.com>
2021-12-04 13:33:59 -0500

    fifthinitial

--- index.js
+++ index.js
@@ -29,6 +29,25 @@ const something = () => {
 //     console.log(document.querySelector('.heading'))
 // }
 
-
 something()
+
+
+
+// const hamburger= document.querySelector(".hamburger");
+// const closeIcon= document.querySelector(".closeIcon");
+// const menuIcon = document.querySelector(".menuIcon");
+
+// function toggleMenu() {
+//   if (menu.classList.contains("showMenu")) {
+//     menu.classList.remove("showMenu");
+//     closeIcon.style.display = "none";
+//     menuIcon.style.display = "block";
+//   } else {
+//     menu.classList.add("showMenu");
+//     closeIcon.style.display = "block";
+//     menuIcon.style.display = "none";
+//   }
+// }
+
+// hamburger.addEventListener("click", toggleMenu);
 
